f2c0884eb63a62e950520d1c300c006f24a8089e
Scott Green <sagreenxyz@gmail.com>
2022-03-23 10:19:32 -0500

    4. adding divs to show basic structure and form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@
 // Import components
 import './App.css';
 
-function App(){
+function App() {
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <div className="Searchbar">
+          <p>Searchbar</p>
+        </div>
+        <div className="Directory">
+          <p>Directory</p>
+        </div>
+        <div className="Gallery">
+          <p>Gallery</p>
+          <div className="Posting">
+            <p>Posting</p>
+          </div>
+        </div>
+        <div className="Sidebar">
+          <p>Sidebar</p>
+          <div className="Help">
+            <p>Help</p>
+          </div>
+        </div>
       </div>
     </div>
   );
